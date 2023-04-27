59dc2cb4411180c533a4976082fbb4bc00da9528
RaulGS <119356579+Wisiwy@users.noreply.github.com>
2023-04-27 21:03:43 +0200

    Examen2021 baja atleta

--- Examenes_Ev3/src/Exam20_21/GestionDB.java
+++ Examenes_Ev3/src/Exam20_21/GestionDB.java
@@ -7,48 +7,44 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 
+import aUtilidad.CreateDB;
 import aUtilidad.Dibujo;
 import aUtilidad.Input;
+import aUtilidad.Leer;
 
 public class GestionDB {
 	public static void main(String[] args) {
 
-		//test con base datos anterior
+		// test con base datos anterior
 		File f, fsql;
 		if (Input.checkLinux()) {
 			f = new File("files/conexion.txt");
 		} else {
 			f = new File("files\\conexion.txt");
 		}
-		
+
 		try {
 			Connection conn = leerConexionBD(f);
-			
-			
-		} catch (IOException | SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	
-		
-		//
-		//menu gestion
-		String[] opc = { "Alta atleta", "Baja de atleta", "Modificacion de valores", "Listado por prueba y marca",
-				"Listado por apellidos y nombres" };
-		int key = Dibujo.menu(opc);
-//		switch (key) {
-//		case 1: {
-//			
-//			
-//			altaAtleta(conn);
-//			yield type;
-//		}
-//		case 2: {
-//
-//			yield type;
-//		}
+
+			//
+			// menu gestion
+			String[] opc = { "Alta atleta", "Baja de atleta", "Modificacion de valores", "Listado por prueba y marca",
+					"Listado por apellidos y nombres" };
+			int key = Dibujo.menu(opc);
+			switch (key) {
+			case 1: {
+				
+				altaAtleta(conn);
+				break;
+			}
+			case 2: {
+				bajaAtleta(conn);
+				break;
+			}
 //		case 3: {
 //
 //			yield type;
@@ -67,8 +63,57 @@ public class GestionDB {
 //		}
 //		default:
 //			throw new IllegalArgumentException("Unexpected value: " + key);
-//		}
-//
+			}
+		
+			conn.close();
+		} catch (IOException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+	}
+
+	private static void bajaAtleta(Connection conn) throws SQLException {
+		String[] pk =  elegirPKAtleta(conn);
+		Statement sente = conn.createStatement( );
+		sente.executeQuery("DELETE FROM atleta WHERE nombre = "+pk[0]+"AND apellido="+pk[1]+ "AND prueba=" + pk[2]+";");
+	}
+
+	private static String[] elegirPKAtleta(Connection conn) {
+		CreateDB.selectTabla(conn, "atleta");
+		String nom = Leer.leerString("Nombre: ").toUpperCase();
+		String ape = Leer.leerString("Apellido:").toUpperCase();
+		String prueba = Leer.leerString("Prueba:").toUpperCase();
+		String[] aux = {nom, ape, prueba};
+		return aux;
+	}
+
+	private static void altaAtleta(Connection conn) throws SQLException {
+		Atleta aux = crearAtleta();
+		String insert = "INSERT INTO atleta VALUES (?,?,?,?,?,?)";
+		PreparedStatement ps = conn.prepareStatement(insert);
+
+		ps.setString(1, aux.getNom());
+		ps.setString(2, aux.getApe());
+		ps.setString(3, aux.getSexo());
+		ps.setString(4, aux.getFecha_nac());
+		ps.setString(5, aux.getPrueba());
+		ps.setString(6, aux.getMarca());
+
+		ps.executeUpdate();
+		System.out.println("Alta realizada!!");
+	}
+
+	private static Atleta crearAtleta() {
+		Dibujo.titulo("ALTA ALTETA");
+		String nom = Leer.leerString("Nombre: ");
+		String ape = Leer.leerString("Apellido: ");
+		String sexo = Leer.leerString("Sexo: ");
+		String fecha_nac = Leer.leerString("Fecha_nac :");
+		String prueba = Leer.leerString("Prueba: ");
+		String marca = Leer.leerString("Marca: ");
+
+		return new Atleta(nom, ape, sexo, fecha_nac, prueba, marca);
 	}
 
 	private static Connection leerConexionBD(File f) throws IOException, SQLException {
@@ -76,15 +121,15 @@ public class GestionDB {
 		String path = null, user = null, pass = null;
 		int cont = 1;
 		String linea = br.readLine();
-		while(linea!= null) {
-			if(cont==1)
+		while (linea != null) {
+			if (cont == 1)
 				path = linea;
-			if(cont==2)
+			if (cont == 2)
 				user = linea;
-			if(cont==3)
+			if (cont == 3)
 				pass = linea;
-			
-			linea=br.readLine()	;
+
+			linea = br.readLine();
 			cont++;
 		}
 		System.out.println("DATOS DE CONEXION");
@@ -92,10 +137,10 @@ public class GestionDB {
 		System.out.println(user);
 		System.out.println(pass);
 		System.out.println(cont + "lineas txt");
-		
+
 		Connection conn = DriverManager.getConnection("jdbc:sqlite:" + path);
 		System.out.println("Conexion realizada");
-		return  conn ;
+		return conn;
 	}
 
 }
--- Examenes_Ev3/src/aUtilidad/CreateDB.java
+++ Examenes_Ev3/src/aUtilidad/CreateDB.java
@@ -6,6 +6,8 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -38,4 +40,39 @@ public static void leerSQL(Connection con,File fentrada) throws SQLException, IO
 		sentencia.close();
 		
 	}
+
+public static void selectTabla(Connection conn,String tabla) {
+	String cab = "";
+	try {
+		Statement sentencia = conn.createStatement();
+		ResultSet res = sentencia.executeQuery("SELECT * FROM " + tabla);
+		ResultSetMetaData rsmd = res.getMetaData();
+		int columnsNumber = rsmd.getColumnCount();
+		// dibuja cabecera
+		for (int i = 1; i <= columnsNumber; i++) {
+			cab += String.format("| %-10s", rsmd.getColumnName(i).toUpperCase());
+		}
+		System.out.println(cab);
+		for (int i = 0; i < cab.length(); i++)
+			System.out.print("-");
+		System.out.println("");
+		// dibuja datos
+		while (res.next()) {
+			for (int i = 1; i <= columnsNumber; i++) {
+				String columnValue = res.getString(i);
+				System.out.printf("| %-10s", columnValue);
+
+			}
+			System.out.println();
+		}
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	for (int i = 0; i < cab.length(); i++)
+		System.out.print("-");
+	System.out.println();
+}
+
+
 }
